4ebe0744caeab0a60432445014de001bd37b2915
konstantin23 <kostharl@gmail.com>
2017-12-26 09:25:36 +0200

    Initial commit

---
+++ client/rollup.config.js
@@ -0,0 +1,36 @@
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
+import uglify from 'rollup-plugin-uglify-es';
+
+export default {
+    entry: './client/index.ts',
+    dest: 'index.js',
+    format: 'cjs',
+    plugins: [
+        nodeResolve({
+            jsnext: true,
+            main: true,
+            preferBuiltins: true
+        }),
+        typescript(require('typescript')),
+        commonjs({
+            extensions: ['.js', '.ts']
+        }),
+        builtins(),
+        uglify({
+            mangle: true,
+            compress: {
+                sequences: true,
+                dead_code: true,
+                conditionals: true,
+                booleans: true,
+                unused: true,
+                if_return: true,
+                join_vars: true,
+                drop_console: false
+            }
+        }),
+    ]
+}
