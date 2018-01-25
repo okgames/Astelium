9be31e58779159c4d8bc6cb5cae1a6d4961a7121
kokh0716 <konstantin.kharlambov@netcracker.com>
2018-01-25 20:03:23 +0200

    Port and host are retrieving from server to the client now

--- client/index.ts
+++ client/index.ts
@@ -12,72 +12,80 @@ import AsteliumPhysicsManager from "client/data/astelium-physicsmanager";
 
 
 document.addEventListener("DOMContentLoaded", () => {  
+    
+    fetch('/app-url',
+    {
+        method: 'GET'
+    })
+    .then((res) => {            
+        return res.json(); 
+    })
+    .then((data) => {      
+        const socket = new WebSocket(`ws://${data.host}:${data.port}`);       
+        socket.onopen = (event) => {           
+            console.log('Connected...');      
+            socket.send(JSON.stringify({type: 'connection-request', status: 'Opened'}));            
+            socket.onmessage = (event) => {           
+                const serverData = JSON.parse(event.data);           
+                console.log('From server: ', serverData);
+                APP_ENGINE_INSTANCE.SOCKET_SERVER_MESSAGES.get(serverData.type)(serverData);         
+            };   
+            window.onbeforeunload = (evt) => {
+                APP_ENGINE_INSTANCE.activePlayers.splice(APP_ENGINE_INSTANCE.activePlayers
+                    .indexOf(APP_ENGINE_INSTANCE.currentPlayer), 1);
+                APP_ENGINE_INSTANCE.availablePlayers.push(APP_ENGINE_INSTANCE.currentPlayer);            
+                socket.send(JSON.stringify({
+                    type: 'before-unload',                
+                    activePlayers: APP_ENGINE_INSTANCE.activePlayers,
+                    availablePlayers: APP_ENGINE_INSTANCE.availablePlayers
+                }));          
+            }                 
+            socket.onclose = (event) => {
+                console.log('Closed connection...');             
+            };    
+            socket.onerror = (error) => {
+                socket.send(JSON.stringify(error));
+            };           
+        }     
+        const audioManager = new AsteliumAudioManager(AsteliumSelector.AUDIO_MANAGER_ID);
+        const stateManager = new AsteliumGameStateManager(AsteliumSelector.GAME_STATE_MANAGER_ID);
+        const physicsManager = new AsteliumPhysicsManager(AsteliumSelector.PHYSICS_MANAGER_ID);  
+        const networkManager = new AsteliumNetworkManager(AsteliumSelector.NETWORK_MANAGER_ID, socket);
+        audioManager.load([
+            '/location.mp3',
+            '/menu.mp3',
+            '/multiplayer.mp3',
+            '/newgame.mp3',
+            '/options.mp3',
+            '/singleplayer.mp3',
+            '/slime.mp3',
+            '/soul.mp3'
+        ]);
 
-    const socket = new WebSocket("ws://localhost:3000");
-    const audioManager = new AsteliumAudioManager(AsteliumSelector.AUDIO_MANAGER_ID);
-    const stateManager = new AsteliumGameStateManager(AsteliumSelector.GAME_STATE_MANAGER_ID);
-    const physicsManager = new AsteliumPhysicsManager(AsteliumSelector.PHYSICS_MANAGER_ID);
-    const networkManager = new AsteliumNetworkManager(AsteliumSelector.NETWORK_MANAGER_ID, socket);
-    audioManager.load([
-        '/location.mp3',
-        '/menu.mp3',
-        '/multiplayer.mp3',
-        '/newgame.mp3',
-        '/options.mp3',
-        '/singleplayer.mp3',
-        '/slime.mp3',
-        '/soul.mp3'
-    ]);
+        APP_ENGINE_INSTANCE.registerManagers([
+            audioManager, stateManager, networkManager, physicsManager
+        ]);
+    
 
-    APP_ENGINE_INSTANCE.registerManagers([
-        audioManager, stateManager, networkManager, physicsManager
-    ]);
-   
-   
-    socket.onopen = (event) => {           
-        console.log('Connected...');      
-        socket.send(JSON.stringify({type: 'connection-request', status: 'Opened'}));            
-        socket.onmessage = (event) => {           
-            const serverData = JSON.parse(event.data);           
-            console.log('From server: ', serverData);
-            APP_ENGINE_INSTANCE.SOCKET_SERVER_MESSAGES.get(serverData.type)(serverData);         
-        };   
-        window.onbeforeunload = (evt) => {
-            APP_ENGINE_INSTANCE.activePlayers.splice(APP_ENGINE_INSTANCE.activePlayers
-                .indexOf(APP_ENGINE_INSTANCE.currentPlayer), 1);
-            APP_ENGINE_INSTANCE.availablePlayers.push(APP_ENGINE_INSTANCE.currentPlayer);            
-            socket.send(JSON.stringify({
-                type: 'before-unload',                
-                activePlayers: APP_ENGINE_INSTANCE.activePlayers,
-                availablePlayers: APP_ENGINE_INSTANCE.availablePlayers
-            }));          
-        }                 
-        socket.onclose = (event) => {
-            console.log('Closed connection...');             
-        };    
-        socket.onerror = (error) => {
-            socket.send(JSON.stringify(error));
-        };           
-    }      
+        APP_ENGINE_INSTANCE.loadModels([
+            new AsteliumGameLayout(AsteliumSelector.GAME_LAYOUT_ID, null, true),
+            new AsteliumMenu(AsteliumSelector.GAME_MENU_ID, null, true),                     
+            new Advicer(
+                AsteliumSelector.ADVICER_ID, null, false,
+                {
+                    x: 250,
+                    y: 250
+                }
+            )    
+        ]);
+    
+        APP_ENGINE_INSTANCE.getModel<AsteliumGameLayout>(AsteliumSelector.GAME_LAYOUT_ID)
+            .setChildModels([
+                APP_ENGINE_INSTANCE.getModel(AsteliumSelector.GAME_MENU_ID),      
+                APP_ENGINE_INSTANCE.getModel(AsteliumSelector.ADVICER_ID)
+            ]);       
 
-    APP_ENGINE_INSTANCE.loadModels([
-        new AsteliumGameLayout(AsteliumSelector.GAME_LAYOUT_ID, null, true),
-        new AsteliumMenu(AsteliumSelector.GAME_MENU_ID, null, true),                     
-        new Advicer(
-            AsteliumSelector.ADVICER_ID, null, false,
-            {
-                x: 250,
-                y: 250
-            }
-        )    
-    ]);
-   
-    APP_ENGINE_INSTANCE.getModel<AsteliumGameLayout>(AsteliumSelector.GAME_LAYOUT_ID)
-        .setChildModels([
-            APP_ENGINE_INSTANCE.getModel(AsteliumSelector.GAME_MENU_ID),      
-            APP_ENGINE_INSTANCE.getModel(AsteliumSelector.ADVICER_ID)
-        ]);       
-
-    APP_ENGINE_INSTANCE.getModel<AsteliumGameLayout>(AsteliumSelector.GAME_LAYOUT_ID)
-        .render('renderTarget');    
+        APP_ENGINE_INSTANCE.getModel<AsteliumGameLayout>(AsteliumSelector.GAME_LAYOUT_ID)
+            .render('renderTarget');      
+    })         
 });
